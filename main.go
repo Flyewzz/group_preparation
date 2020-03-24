5f8fb8c1c721163d8e32b238bf182a805e46a56d
Flyewzz <dentalon599@gmail.com>
2020-03-24 16:59:04 +0300

    CORS was added

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Flyewzz/group_preparation/handlers"
+	"github.com/rs/cors"
 	"github.com/spf13/viper"
 )
 
@@ -13,6 +14,15 @@ func main() {
 	r := NewRouter()
 	HandlerData := PrepareHandlerData()
 	handlers.ConfigureHandlers(r, HandlerData)
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{
+			"www.google.ru",
+		},
+		AllowCredentials: true,
+		// Enable Debugging for testing, consider disabling in production
+		Debug: true,
+	})
+	corsHandler := c.Handler(r)
 	fmt.Println("Server is listening...")
-	http.ListenAndServe(":"+viper.GetString("port"), r)
+	http.ListenAndServe(":"+viper.GetString("port"), corsHandler)
 }
